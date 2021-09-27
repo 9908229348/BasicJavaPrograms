9fc090d36cdc261849c95a9398d68289d6cbb725
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-27 18:54:24 +0530

    Added: Program to find Nth Harmonic Value

---
+++ HarmonicNumber.java
@@ -0,0 +1,19 @@
+package basicjavaprograms;
+
+import java.util.Scanner;
+
+public class HarmonicNumber {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a Number :");
+		int valueN = scanner.nextInt();
+		double harmonic = 0;
+		int count;
+		for(count = 1;count <= valueN;count++)
+		{
+			harmonic = harmonic +(double) 1 / count;
+		}
+	System.out.println(valueN + " Harmonic value is : " + harmonic);
+	}
+}
+
